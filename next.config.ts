53ef435f74aa0721d2ee74620f6fdbcc9117e028
jiyong <parkjiy96@naver.com>
2025-12-04 19:33:17 +0900

    초기개발환경 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), "src")],
+    additionalData: '@use "@/styles/_mixins.scss" as *;',
+  },
 };
 
 export default nextConfig;
