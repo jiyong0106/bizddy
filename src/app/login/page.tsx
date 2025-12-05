9dcdbbfcb6b366628b8908358df4981e2c69b495
jiyong <parkjiy96@naver.com>
2025-12-05 18:42:44 +0900

    랜딩페이지기초 헤더 생성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,5 @@
 const LoginPage = () => {
-  return (
-    <div>
-      <header>여기는 헤더</header>
-      <main>여기는 메인</main>
-      <footer>여기는 푸터</footer>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default LoginPage;
--- src/components/landing/landing-header/index.tsx
+++ src/components/landing/landing-header/index.tsx
@@ -1,8 +1,34 @@
+import Link from "next/link";
 import styles from "./landing-header.module.scss";
 import clsx from "clsx";
 
 const LandingHeader = () => {
-  return <header className={clsx(styles.container)}>LandingHeader</header>;
+  const menuItems = [
+    { label: "Home", href: "/" },
+    { label: "Features", href: "/features" },
+    { label: "Pricing", href: "/pricing" },
+    { label: "Contact", href: "/contact" },
+  ];
+
+  return (
+    <header className={clsx(styles.container)}>
+      <div className={styles.inner}>
+        <div className={styles.logo}>bizddy</div>
+
+        <nav className={styles.nav}>
+          <ul className={styles.menu}>
+            {menuItems.map((item) => (
+              <li className={styles.menuItem}>{item.label}</li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className={styles.actions}>
+          <Link href={"/login"}>시작하기</Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default LandingHeader;
