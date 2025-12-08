7ea97416cbbde22d8a0b682449cf4803cdde8271
jiyong <parkjiy96@naver.com>
2025-12-08 20:53:01 +0900

    기본 레이아웃 1차

--- src/components/spaces/topbar/index.tsx
+++ src/components/spaces/topbar/index.tsx
@@ -1,15 +1,23 @@
 import styles from "./topbar.module.scss";
+import { IoListOutline, IoCloseOutline } from "react-icons/io5";
 
 interface Props {
   onMoreClick: () => void;
+  open: boolean;
 }
 
-const Topbar = ({ onMoreClick }: Props) => {
+const Topbar = ({ onMoreClick, open }: Props) => {
+  const Icon = open ? IoCloseOutline : IoListOutline;
+
   return (
     <div className={styles.container}>
-      <button type="button" className={styles.moreBtn} onClick={onMoreClick}>
-        더보기
-      </button>
+      <Icon
+        className={styles.moreIcon}
+        onClick={onMoreClick}
+        aria-label="메뉴 열기/닫기"
+      />
+
+      <p>박지용</p>
     </div>
   );
 };
