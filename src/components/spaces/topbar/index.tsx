3f45a76e2a0368934e93fe523da583f2e755bcb4
jiyong <parkjiy96@naver.com>
2025-12-08 20:18:59 +0900

    layout반응형

--- src/components/spaces/topbar/index.tsx
+++ src/components/spaces/topbar/index.tsx
@@ -7,9 +7,8 @@ interface Props {
 const Topbar = ({ onMoreClick }: Props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.brand}>bizddy</div>
       <button type="button" className={styles.moreBtn} onClick={onMoreClick}>
-        더보
+        더보기
       </button>
     </div>
   );
