8e595f294e2d11ce0434c56f6b4028307d908761
jiyong <parkjiy96@naver.com>
2025-12-12 20:13:28 +0900

    2차 테스트

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import styles from "./dashboard-layout.module.scss";
 import { ClientShell } from "@/components/dashboard";
 
-export const dynamic = "force-static";
+// export const dynamic = "force-static";
 
 export default function DashboardLayout({
   children,
