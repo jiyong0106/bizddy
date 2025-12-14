9b8acd3cf22fb9a66fcae2805d832d62155a11c8
jiyong <parkjiy96@naver.com>
2025-12-15 02:27:37 +0900

    dorpdown 컴포넌트 생성

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -2,10 +2,12 @@ import styles from "./navbar.module.scss";
 import { Logo } from "@/components/common";
 import Plan from "../plan";
 import NavbarSection from "../navbar-section";
+import PlanDropdown from "../plan-dorpdown";
 
 const Navbar = () => {
   return (
     <nav className={styles.container}>
+      <PlanDropdown />
       <div className={styles.topSection}>
         <Logo size={24} />
         <Plan />
