6c3fe6efc78205484f5eebe77259e8f141159d4e
jiyong <parkjiy96@naver.com>
2025-12-18 19:17:34 +0900

    드랍다운 컴포넌트 위치 수정

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,16 +1,23 @@
+"use client";
 import styles from "./navbar.module.scss";
 import { Logo } from "@/components/common";
 import Plan from "../plan";
 import NavbarSection from "../navbar-section";
-import PlanDropdown from "../plan-dorpdown";
+import PlanDropdown from "../plan-dropdown";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className={styles.container}>
-      <PlanDropdown />
       <div className={styles.topSection}>
         <Logo size={24} />
-        <Plan />
+        <Plan setOpen={setOpen} />
+        {open && (
+          <div className={styles.dropdown}>
+            <PlanDropdown open={open} />
+          </div>
+        )}
       </div>
       <NavbarSection />
     </nav>
